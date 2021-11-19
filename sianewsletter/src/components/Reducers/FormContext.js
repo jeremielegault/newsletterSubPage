84898f3ab5ac196cd1cae936ffe474ebb337b863
“Jeremie <jeremie_legault@hotmail.com>
2021-11-19 13:14:24 -0500

    first commit

---
+++ sianewsletter/src/components/Reducers/FormContext.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const FormContext = React.createContext();
+
+// This is what the object that contains all the user input looks like.
+const initialState = {
+  name: "",
+  company: "",
+  email: "",
+  checkbox: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "receive-form-info": {
+      return {
+        ...state,
+        name: action.name,
+        company: action.company,
+        email: action.email,
+        checkbox: action.checkbox,
+      };
+    }
+
+    default:
+      throw new Error(`unrecognized action: ${action.type}`);
+  }
+}
+
+export const FormProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const receiveFormInfo = (data) => {
+    dispatch({
+      type: "receive-form-info",
+      ...data,
+    });
+  };
+
+  return (
+    <FormContext.Provider
+      value={{
+        state,
+        receiveFormInfo,
+      }}
+    >
+      {children}
+    </FormContext.Provider>
+  );
+};
+
+export default FormContext;
---
+++ sianewsletter/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { FormProvider } from "./components/Reducers/FormContext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FormProvider>
+      <App />
+    </FormProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
